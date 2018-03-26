67d9a8211f5c2e364fe8fbcbedc7285acb72295d
Larionov Anton <larionov.anton85@gmail.com>
2018-03-26 13:24:23 +0400

    My games

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+       
+    {
+        Logic logic = new Logic();
+        bool comp_x = false;
+        bool comp_o = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            start_game();
+
+        }
+
+        private void start_game()
+        {
+            logic.Init();
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+            pictureBox4.Image = null;
+            pictureBox5.Image = null;
+            pictureBox6.Image = null;
+            pictureBox7.Image = null;
+            pictureBox8.Image = null;
+            pictureBox9.Image = null;
+            
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            start_game();
+        }
+
+        private void menu_Exit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            make_move((PictureBox)sender);
+        }
+
+        private void make_move(PictureBox box)
+        {
+            int x, y;
+            string tag = box.Tag.ToString();
+
+            x = int.Parse(tag.Substring(0, 1));
+            y = int.Parse(tag.Substring(1, 1));
+
+            int side = logic.side;
+            if (logic.Place(x, y))
+            {
+
+                box.Image = (side == 1) ? Properties.Resources.krest : Properties.Resources.nolik;
+            }
+
+            if (logic.finish != "play") { game_over(); return; }
+        }
+
+
+            private void make_Comp()
+        {
+            int x, y;
+            int side = logic.side;
+            logic.Comp(out x, out y);
+
+            picture(x, y).Image = (side == 1) ? Properties.Resources.krest : Properties.Resources.nolik;
+
+            if (logic.finish != "play") { game_over(); return; }
+        }
+
+
+
+        private void game_over()
+        {
+            switch (logic.finish)
+            {
+                case "winx": MessageBox.Show ("Победили крестики"); return;
+                case "wino": MessageBox.Show ("Победили нолики"); return;
+                case "draw": MessageBox.Show ("Ничья"); return;
+                default: return;
+            }
+        }
+
+        private void menu_start_comp_Click(object sender, EventArgs e)
+        {
+            start_game();
+            comp_o = false;
+            comp_x = true;
+            make_Comp();
+        
+       
+
+        }
+
+        private void menu_play_With_comp_x_Click(object sender, EventArgs e)
+        {
+            comp_o = true;
+            comp_x = false;
+        }
+
+       private PictureBox picture (int x, int y)
+        {
+            if (x == 0 && y == 0) return pictureBox1;
+            if (x == 0 && y == 1) return pictureBox2;
+            if (x == 0 && y == 2) return pictureBox3;
+            if (x == 1 && y == 0) return pictureBox4;
+            if (x == 1 && y == 1) return pictureBox5;
+            if (x == 1 && y == 2) return pictureBox6;
+            if (x == 2 && y == 0) return pictureBox7;
+            if (x == 2 && y == 1) return pictureBox8;
+            if (x == 2 && y == 2) return pictureBox9;
+            return null;
+
+        }
+
+    }
+}
---
+++ WindowsFormsApplication2/Logic.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class Logic
+    {
+        Random rnd = new Random();
+        int[,] map = new int[3, 3];
+        public int side { get; private set; }
+           int step;
+        bool play;
+        public string finish { get; private set; } 
+        public Logic()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            for (int x = 0; x < 3; x++)
+                for (int y = 0; y < 3; y++)
+                {
+                    map[x, y] = 0;
+                }
+
+            side = 1;
+            play = true;
+            step = 0;
+        }
+
+        public bool Place(int x, int y)
+        {
+            if (!play) return false;
+            if (x < 0 || x > 3) return false;
+            if (y < 0 || y > 3) return false;
+            if (map[x, y] > 0) return false;
+            map[x, y] = side;
+            step++;
+            finish = Finish(x, y);
+            if (side == 1) side = 2; else side = 1;
+
+
+            return true;
+            
+        }
+
+        private string Finish (int x, int y)
+        {
+
+            bool win = false;
+            if (map[x, 0] == side && map[x, 1] == side && map[x, 2] == side) win = true;
+            if (map[0, y] == side && map[1, y] == side && map[2, y] == side) win = true;
+            if (map[0, 0] == side && map[1, 1] == side && map[2, 2] == side) win = true;
+            if (map[0, 2] == side && map[1, 1] == side && map[2, 0] == side) win = true;
+
+            if (win)
+            {
+                if (side == 1) { play = false; return "winx"; } else { play = false; return "wino"; }
+
+            }
+            else 
+            {
+                if (side == 9) { play = false; return "draw"; } else { return "play"; }
+            }
+
+
+        }
+
+        public void Comp(out int x, out int y)
+        {
+
+            int loop = 100;
+            do
+            {
+                x = rnd.Next(0, 3);
+                y = rnd.Next(0, 3);
+            }
+            while (map[x, y] > 0 && --loop > 0);
+            Place(x, y);
+        }
+
+
+    }
+}
